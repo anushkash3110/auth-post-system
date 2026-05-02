fae60edc7878bba76fdaf25c9bc88de295148331
Anushka Sharma <anush2007ka@gmail.com>
2026-05-02 10:50:29 +0530

    added index and downloaded packages

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const { name } = require('ejs');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/auth-post-system")
+
+const userSchema = mongoose.Schema({
+    username: String,
+    name: String,
+    age: Number,
+    email: String,
+    password: String
+});
+
+
+module.exports = mongoose.model('user', userSchema);
