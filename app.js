01f780fca82c2407075fa183a39fd42a3d33ec71
Anushka Sharma <anush2007ka@gmail.com>
2026-05-03 16:30:06 +0530

    user details submission in server

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const userModel = require("./models/user");
 const cookieParser = require('cookie-parser');
+const bcryt = require('bcrypt');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -11,5 +12,28 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+
+app.post('/register', async (req, res) => {
+    let {name, username, email, password} = req.body;
+
+    let user = await userModel.findOne({email});
+    if(user){
+        return res.status(500).send("User already exists");
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            let user = await userModel.create({
+                name,
+                username,
+                age,
+                email,
+                password: hash
+            });
+        });
+    });
+});
+
 
 app.listen(3001);
