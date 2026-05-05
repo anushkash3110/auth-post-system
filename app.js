5627d4b6b8224619c2e325d2b9c4990c228411a3
Anushka Sharma <anush2007ka@gmail.com>
2026-05-05 19:13:27 +0530

    file upload initialized

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
-const e = require('express');
+const crypto = require('crypto');
+const path = require('path');
+const multer = require('multer');
+
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -14,10 +17,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/upload')
+  },
+  filename: function (req, file, cb) {
+    
+crypto.randomBytes(12, function(err, bytes) {
+    const fn = bytes.toString("hex") + path.extname(file.originalname);
+     cb(null, fn)
+    });
+  }
+})
+
+const upload = multer({ storage: storage })
+
+
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+
+app.get('/test', (req, res) => {
+    res.render("test");
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+    console.log(req.file);
+});
 
 app.get('/login', (req, res) => {
     res.render('login');
