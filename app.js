863e59305b961489cff9e4eeb20a3143912be39a
Anushka Sharma <anush2007ka@gmail.com>
2026-05-06 05:07:32 +0530

    almost ready

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 const path = require('path');
-const multerconfig = require('./config/multerconfig');
+const upload = require('./config/multerconfig');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -22,6 +22,16 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/profile/upload', (req, res) => {
+    res.render("profileupload");
+});
+
+app.get('/upload', upload.single("image"), async (req, res) => {
+    let user = await userModel.findOne({email: req.user.email});
+    user.profilepic = req.file.filename;
+    await user.save();
+    res.redirect("/profile");
+});
 
 
 app.get('/login', (req, res) => {
--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = mongoose.Schema({
     password: String,
     profilepic: {
         type: String,
-        default: "profile.jpg"
+        default: "default.jpg"
     },
     posts: [{
         type: mongoose.Schema.Types.ObjectId,
